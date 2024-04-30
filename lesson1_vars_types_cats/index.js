6f96b07bed914ba8948f82c426b7fd27758b8826
sashalehedza <sashalehedza@gmail.com>
2024-04-30 13:28:35 +0300

    task 1

---
+++ lesson1_vars_types_cats/index.js
@@ -0,0 +1,55 @@
+'use strict';
+
+// task 1
+let hello = 'hello'
+let owu = 'owu'
+let com = 'com'
+let ua = 'ua'
+let one = 1
+let ten = 10
+let minus3nine = -999
+let oneTwoThree = 123
+let p = 3.14
+let r = 2.7
+let sixteen = 16
+let trueValue = true
+let falseValue = false
+
+console.log(hello)
+console.log(owu)
+console.log(com)
+console.log(ua)
+console.log(one)
+console.log(ten)
+console.log(minus3nine)
+console.log(oneTwoThree)
+console.log(p)
+console.log(r)
+console.log(sixteen)
+console.log(trueValue)
+console.log(falseValue)
+
+// task 2
+
+let firstName = 'Oleksandr'
+let lastName = 'Lehedza'
+let middleName= 'Romanovich'
+
+console.log(lastName + ' ' + firstName + ' ' + middleName)
+
+// task 3
+let a = 100
+let b = '100'
+let c = true
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+
+// extra task
+let firstNamePrompt = prompt('firstName')
+let lastNamePrompt = prompt('lastName')
+let agePrompt = prompt('age')
+
+console.log(firstNamePrompt)
+console.log(lastNamePrompt)
+console.log(agePrompt)
