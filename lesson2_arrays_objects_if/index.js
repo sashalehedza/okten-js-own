115fcd5cca104e802b35b2892a8f3863cada4588
sashalehedza <sashalehedza@gmail.com>
2024-05-02 10:23:03 +0300

    task 2 js

--- lesson2_arrays_objects_if/index.js
+++ lesson2_arrays_objects_if/index.js
@@ -441,10 +441,11 @@ if (a > b) {
 // - є змінна х, яка може прийняти будь-яке значення (стрінг, число, undefined, null  і тд включно). Напишіть код який,
 //  буде присвоювати змінній х значення "default"  якщо ви намагаєтесь присвоїти в неї falsy-значення (хибноподібні, тобто ті, які приводиться до false, а це 0 null undefined і тд).
 
-const randomTypeValue = prompt('Enter value')
+let randomTypeValue = prompt('Enter value')
 
 if (!!randomTypeValue) {
-  console.log('default')
+  randomTypeValue = 'default'
+  console.log(randomTypeValue)
 } else {
   console.log(randomTypeValue)
 }
